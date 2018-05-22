9f0d23c45c503f4a6c611bf630c1ea223e3612c4
BrunoSMachado <bruno_machadinho5@hotmail.com>
2018-05-22 14:37:12 +0100

    Alterações na wallet e apagar ficheiros não necessários

--- wallet/src/js/overview_load.js
+++ wallet/src/js/overview_load.js
@@ -3,19 +3,7 @@ const connection = require('./connection.js')
 const dialog = remote.dialog
 
 // input
-const browseButton = document.getElementById('button-load-browse')
-const walletPath = document.getElementById('input-load-path')
-const walletPassword = document.getElementById('input-load-password')
-
-browseButton.addEventListener('click', function(event) {
-    dialog.showOpenDialog({properties: ['openFile']}, function (files) {
-        if (files) {
-            // only one file may be selected
-            walletPath.value = files[0]
-        }
-    })
-})
-
+const public_key = document.getElementById('input-load-public')
 const loadButton = document.getElementById('button-load-load')
 const errorText = document.getElementById('text-load-error')
 const warningText = document.getElementById('text-load-warning')
@@ -27,29 +15,72 @@ loadButton.addEventListener('click', function (event) {
     warningText.innerHTML = ''
     successText.innerHTML = ''
 
-    // display error message in case of failure
-    function onFailure(errorMessage) {
-        warningText.innerHTML = ''
-        errorText.innerHTML = errorMessage
-    }
+    var Fabric_Client = require('fabric-client');
+    var path = require('path');
+    var util = require('util');
+    var os = require('os');
+    //
+    var fabric_client = new Fabric_Client();
 
-    // display success
-    function onSuccess(errorMessage) {
-        warningText.innerHTML = ''
-        successText.innerHTML = 'Wallet successfully loaded! Switch to the overview tab to check your balance.'
-        /*const overviewButton = document.getElementById('button-section-overview')
-        overviewButton.click()*/
-    }
+    // setup the fabric network
+    var channel = fabric_client.newChannel('mychannel');
+    var peer = fabric_client.newPeer('grpc://localhost:7051');
+    channel.addPeer(peer);
 
-    // terminate walletd in case it's running
-    // and after that, spawn the new walletd
-    warningText.innerHTML = 'Saving...'
-    connection.terminateWallet().then((v) => {
-        warningText.innerHTML = 'Loading wallet file...'
-        // this timeout fixed an issue that happened when loading
-        // a wallet that was already loaded
-        setTimeout(function(){
-            connection.spawnWallet(walletPath.value, walletPassword.value, onFailure, onSuccess)
-        }, 1000)
-    })
-})
+    //
+    var member_user = null;
+    var store_path = path.join(__dirname, 'hfc-key-store');
+    console.log('Store path:'+store_path);
+    var tx_id = null;
+
+    // create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
+    Fabric_Client.newDefaultKeyValueStore({ path: store_path
+    }).then((state_store) => {
+    	// assign the store to the fabric client
+    	fabric_client.setStateStore(state_store);
+    	var crypto_suite = Fabric_Client.newCryptoSuite();
+    	// use the same location for the state store (where the users' certificate are kept)
+    	// and the crypto store (where the users' keys are kept)
+    	var crypto_store = Fabric_Client.newCryptoKeyStore({path: store_path});
+    	crypto_suite.setCryptoKeyStore(crypto_store);
+    	fabric_client.setCryptoSuite(crypto_suite);
+
+    	// get the enrolled user from persistence, this user will sign all requests
+    	return fabric_client.getUserContext('user1', true);
+    }).then((user_from_store) => {
+    	if (user_from_store && user_from_store.isEnrolled()) {
+    		console.log('Successfully loaded user1 from persistence');
+    		member_user = user_from_store;
+    	} else {
+    		throw new Error('Failed to get user1.... run registerUser.js');
+    	}
+
+    	// queryCar chaincode function - requires 1 argument, ex: args: ['CAR4'],
+    	// queryAllCars chaincode function - requires no arguments , ex: args: [''],
+    	const request = {
+    		//targets : --- letting this default to the peers assigned to the channel
+    		chaincodeId: 'NewsCoin',
+    		fcn: 'queryPeer',
+    		args: [public_key]
+    	};
+
+    	// send the query proposal to the peer
+    	return channel.queryByChaincode(request);
+    }).then((query_responses) => {
+    	console.log("Query has completed, checking results");
+    	// query_responses could have more than one  results if there multiple peers were used as targets
+    	if (query_responses && query_responses.length == 1) {
+    		if (query_responses[0] instanceof Error) {
+    			console.error("error from query = ", query_responses[0]);
+    		} else {
+          document.getElementById("wallet-address").innerHTML = public_key;
+          document.getElementsById("balance-available").innerHTML = query_responses[0].toString();
+    			console.log("Response is ", query_responses[0].toString());
+    		}
+    	} else {
+    		console.log("No payloads were returned from query");
+    	}
+    }).catch((err) => {
+    	console.error('Failed to query successfully :: ' + err);
+    });
+}
